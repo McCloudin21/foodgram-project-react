298d9ca3f3a9ab7dd3051565aeb1d25cd90333e3
McCloudin21 <seriy293040@yandex.ru>
2024-02-08 06:42:27 +0300

    Corr

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,8 +4,7 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            and )
+            request.method in permissions.SAFE_METHODS)
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):

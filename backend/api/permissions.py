73cd6898075ae13eee52c39c3abe52462a9b039c
McCloudin21 <seriy293040@yandex.ru>
2024-02-09 11:55:24 +0300

    Corr

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -11,5 +11,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             obj.author == request.user
-            or request.method in permissions.SAFE_METHODS 
+            or request.method in permissions.SAFE_METHODS
         )
